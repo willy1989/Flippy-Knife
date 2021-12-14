20805a485177d2d6ee045a45353bfc7b4efd1419
Willy <willy.winelover@gmail.com>
2021-12-14 15:12:24 +0100

    Knife stops when stabbing a wall

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants 
+{
+    public const string Stabbable_Tag = "Stabbable";
+}
--- Assets/Scripts/KnifeMovement.cs
+++ Assets/Scripts/KnifeMovement.cs
@@ -10,7 +10,7 @@ public class KnifeMovement : MonoBehaviour
 
     private bool readyToMove = false;
 
-    private Vector3 movementForceVector = new Vector3(400f, 400f, 0f);
+    private Vector3 movementForceVector = new Vector3(200f, 400f, 0f);
     private Vector3 rotateForceVector = new Vector3(0f, 0f, -350f);
 
     private void Awake()
@@ -35,8 +35,21 @@ public class KnifeMovement : MonoBehaviour
 
     private void Move()
     {
+        if (rigidBody.constraints == RigidbodyConstraints.FreezeAll)
+            UnFreezeMovement();
+
         rigidBody.velocity = Vector3.zero;
         rigidBody.AddTorque(rotateForceVector);
         rigidBody.AddForce(movementForceVector);
+    }
+
+    public void FreezeMovement()
+    {
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.constraints = RigidbodyConstraints.FreezeAll;
+    }
+    private void UnFreezeMovement()
+    {
+        rigidBody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
     }
 }
